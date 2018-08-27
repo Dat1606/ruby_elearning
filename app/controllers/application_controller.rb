bfcf9ab9223320a28548d12c7d375033b2e761d6
Dat1606 <samsunggalaxyvip1@gmail.com>
2018-08-27 16:51:53 +0700

    Log in, Log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  before_action :create_user
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "please_log_in"
+      redirect_to login_url
+    end
+  end
+
+  def create_user
+    @user = User.new
+  end
 end
